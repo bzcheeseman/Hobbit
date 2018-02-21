e85878d6eccef6a0cf41d006c5a33017687b3d1a
aman <aman@unify.id>
2018-02-21 15:01:03 -0800

    clang-format

--- Operator/include/Variable.hpp
+++ Operator/include/Variable.hpp
@@ -1,30 +1,26 @@
 #ifndef HOBBIT_VARIABLE_HPP
 #define HOBBIT_VARIABLE_HPP
 
-
 #include <cstdint>
-#include <llvm/IR/Value.h>
-#include <llvm/IR/DerivedTypes.h>
-#include <llvm/ADT/ArrayRef.h>
-#include <llvm/IR/IRBuilder.h>
 #include <list>
+#include <llvm/ADT/ArrayRef.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Value.h>
 
 namespace Hobbit {
 
-//  llvm::Intrinsic:: is the namespace for intrinsics
+  //  llvm::Intrinsic:: is the namespace for intrinsics
 
   struct Range {
-    Range(const uint32_t &start=0, const uint32_t &end=0) : start(start), end(end) {}
+    Range(const uint32_t &start = 0, const uint32_t &end = 0)
+        : start(start), end(end) {}
 
     uint32_t start;
     uint32_t end; // exclusive
   };
 
-  enum Axis {
-    K,
-    H,
-    W
-  };
+  enum Axis { K, H, W };
 
   // TODO: make dynamic batch size possible with n = 0?
   // TODO: Check indexing to make sure everything is correct
@@ -34,13 +30,14 @@ namespace Hobbit {
 
     uint32_t At(const uint32_t &k, const uint32_t &h, const uint32_t &w);
 
-    Shape GetChunkShape(const Range &k_range, const Range &h_range, const Range &w_range);
-    Range GetChunkIdx(const Range &k_range, const Range &h_range, const Range &w_range);
+    Shape GetChunkShape(const Range &k_range, const Range &h_range,
+                        const Range &w_range);
+    Range GetChunkIdx(const Range &k_range, const Range &h_range,
+                      const Range &w_range);
 
     uint32_t GetSize();
 
     const uint32_t &GetAxisSize(Axis &axis);
-
 
   private:
     const uint32_t k_, h_, w_;
@@ -48,25 +45,32 @@ namespace Hobbit {
 
   class Variable {
   public:
+    // Takes in the shape of the buffer to allocate, allows us to do things like
+    // Pack and Split
+    Variable(llvm::IRBuilder<> &builder, llvm::Type *scalar_type,
+             const Shape &shape);
 
-    // Takes in the shape of the buffer to allocate, allows us to do things like Pack and Split
-    Variable(llvm::IRBuilder<> &builder, llvm::Type *scalar_type, const Shape &shape);
-
-    Variable(llvm::Value *value, llvm::Type *type, const Shape &shape, Variable *parent);
+    Variable(llvm::Value *value, llvm::Type *type, const Shape &shape,
+             Variable *parent);
 
     virtual ~Variable();
 
-    // Should be used to split into arbitrary chunks - will allow you to further split if desired.
-    Variable *GetChunk(llvm::IRBuilder<> &builder, const Range &k_range, const Range &h_range, const Range &w_range);
+    // Should be used to split into arbitrary chunks - will allow you to further
+    // split if desired.
+    Variable *GetChunk(llvm::IRBuilder<> &builder, const Range &k_range,
+                       const Range &h_range, const Range &w_range);
     Variable *Flatten();
 
     void ClearChild(Variable *child);
     void ClearChildren();
 
-    // This function treats the whole array as a flat buffer, and is therefore recommended to only be used
-    // when trying to schedule a computation using SIMD instructions, and therefore used on a very small
+    // This function treats the whole array as a flat buffer, and is therefore
+    // recommended to only be used
+    // when trying to schedule a computation using SIMD instructions, and
+    // therefore used on a very small
     // piece of a variable.
-    llvm::ArrayRef<llvm::Value *> Pack(llvm::IRBuilder<> &builder, uint32_t &vector_size);
+    llvm::ArrayRef<llvm::Value *> Pack(llvm::IRBuilder<> &builder,
+                                       uint32_t &vector_size);
 
   protected:
     llvm::Value *m_value_;
@@ -80,5 +84,4 @@ namespace Hobbit {
   // TODO: Constant object with the same methods as Variable
 }
 
-
-#endif //HOBBIT_VARIABLE_HPP
+#endif // HOBBIT_VARIABLE_HPP
--- Operator/src/Variable.cpp
+++ Operator/src/Variable.cpp
@@ -1,39 +1,49 @@
 #include "Variable.hpp"
 
-Hobbit::Shape::Shape(const uint32_t &k, const uint32_t &h, const uint32_t &w) : k_(k), h_(h), w_(w) {}
+Hobbit::Shape::Shape(const uint32_t &k, const uint32_t &h, const uint32_t &w)
+    : k_(k), h_(h), w_(w) {}
 
-uint32_t Hobbit::Shape::At(const uint32_t &k, const uint32_t &h, const uint32_t &w) {
-  if (k >= k_) throw std::runtime_error("Invalid index, k = " + std::to_string(k) + "geq k_ = " + std::to_string(k_));
-  if (h >= h_) throw std::runtime_error("Invalid index, h = " + std::to_string(h) + "geq h_ = " + std::to_string(h_));
-  if (w >= w_) throw std::runtime_error("Invalid index, w = " + std::to_string(w) + "geq w_ = " + std::to_string(w_));
+uint32_t Hobbit::Shape::At(const uint32_t &k, const uint32_t &h,
+                           const uint32_t &w) {
+  if (k >= k_)
+    throw std::runtime_error("Invalid index, k = " + std::to_string(k) +
+                             "geq k_ = " + std::to_string(k_));
+  if (h >= h_)
+    throw std::runtime_error("Invalid index, h = " + std::to_string(h) +
+                             "geq h_ = " + std::to_string(h_));
+  if (w >= w_)
+    throw std::runtime_error("Invalid index, w = " + std::to_string(w) +
+                             "geq w_ = " + std::to_string(w_));
 
-  return (k*h_ + h)*w_ + w;
+  return (k * h_ + h) * w_ + w;
 }
 
-Hobbit::Shape
-Hobbit::Shape::GetChunkShape(const Hobbit::Range &k_range, const Hobbit::Range &h_range, const Hobbit::Range &w_range) {
-  return {k_range.end - k_range.start, h_range.end - h_range.start, w_range.end - w_range.start};
+Hobbit::Shape Hobbit::Shape::GetChunkShape(const Hobbit::Range &k_range,
+                                           const Hobbit::Range &h_range,
+                                           const Hobbit::Range &w_range) {
+  return {k_range.end - k_range.start, h_range.end - h_range.start,
+          w_range.end - w_range.start};
 }
 
-Hobbit::Range
-Hobbit::Shape::GetChunkIdx(const Hobbit::Range &k_range, const Hobbit::Range &h_range, const Hobbit::Range &w_range) {
-  Range idx_range(0, k_*h_*w_);
+Hobbit::Range Hobbit::Shape::GetChunkIdx(const Hobbit::Range &k_range,
+                                         const Hobbit::Range &h_range,
+                                         const Hobbit::Range &w_range) {
+  Range idx_range(0, k_ * h_ * w_);
   uint32_t k_start, k_end, h_start, h_end, w_start, w_end;
 
   if (k_range.start == 0 && k_range.end == 0) {
     k_start = 0;
     k_end = k_ - 1;
-  }
-  else {
+  } else {
     k_start = k_range.start;
-    k_end = k_range.end - 1; // so if the range is {0, 1} we just have a single k index
+    k_end = k_range.end -
+            1; // so if the range is {0, 1} we just have a single k index
   }
 
   if (h_range.start == 0 && h_range.end == 0) {
     h_start = 0;
     h_end = h_ - 1;
-  }
-  else {
+  } else {
     h_start = h_range.start;
     h_end = h_range.end - 1;
   }
@@ -41,8 +51,7 @@ Hobbit::Shape::GetChunkIdx(const Hobbit::Range &k_range, const Hobbit::Range &h_
   if (w_range.start == 0 && w_range.end == 0) {
     w_start = 0;
     w_end = w_ - 1;
-  }
-  else {
+  } else {
     w_start = w_range.start;
     w_end = w_range.end - 1;
   }
@@ -53,27 +62,32 @@ Hobbit::Shape::GetChunkIdx(const Hobbit::Range &k_range, const Hobbit::Range &h_
   return idx_range;
 }
 
-uint32_t Hobbit::Shape::GetSize() {
-  return k_*h_*w_;
-}
+uint32_t Hobbit::Shape::GetSize() { return k_ * h_ * w_; }
 
 const uint32_t &Hobbit::Shape::GetAxisSize(Hobbit::Axis &axis) {
   switch (axis) {
-    case K: return k_;
-    case H: return h_;
-    case W: return w_;
+  case K:
+    return k_;
+  case H:
+    return h_;
+  case W:
+    return w_;
   }
 
   return 0;
 }
 
-Hobbit::Variable::Variable(llvm::IRBuilder<> &builder, llvm::Type *scalar_type, const Hobbit::Shape &shape) : m_shape_(shape) {
+Hobbit::Variable::Variable(llvm::IRBuilder<> &builder, llvm::Type *scalar_type,
+                           const Hobbit::Shape &shape)
+    : m_shape_(shape) {
   m_type_ = scalar_type;
-  m_value_ = builder.CreateAlloca(m_type_, builder.getInt64(m_shape_.GetSize()));
+  m_value_ =
+      builder.CreateAlloca(m_type_, builder.getInt64(m_shape_.GetSize()));
 }
 
-Hobbit::Variable::Variable(llvm::Value *value, llvm::Type *type, const Hobbit::Shape &shape, Variable *parent) :
-        m_shape_(shape), m_value_(value), m_type_(type), m_parent_(parent) {
+Hobbit::Variable::Variable(llvm::Value *value, llvm::Type *type,
+                           const Hobbit::Shape &shape, Variable *parent)
+    : m_shape_(shape), m_value_(value), m_type_(type), m_parent_(parent) {
   ;
 }
 
@@ -87,15 +101,18 @@ Hobbit::Variable::~Variable() {
   ClearChildren();
 }
 
-Hobbit::Variable *
-Hobbit::Variable::GetChunk(llvm::IRBuilder<> &builder, const Hobbit::Range &k_range, const Hobbit::Range &h_range,
-                           const Hobbit::Range &w_range) {
+Hobbit::Variable *Hobbit::Variable::GetChunk(llvm::IRBuilder<> &builder,
+                                             const Hobbit::Range &k_range,
+                                             const Hobbit::Range &h_range,
+                                             const Hobbit::Range &w_range) {
   Range idx_range = m_shape_.GetChunkIdx(k_range, h_range, w_range);
   Shape chip_shape = m_shape_.GetChunkShape(k_range, h_range, w_range);
 
-  llvm::Value *chip_start_ptr = builder.CreateGEP(m_value_, builder.getInt32(idx_range.start));
+  llvm::Value *chip_start_ptr =
+      builder.CreateGEP(m_value_, builder.getInt32(idx_range.start));
 
-  Variable *output = new Variable (chip_start_ptr, m_type_->getArrayElementType(), chip_shape, this);
+  Variable *output = new Variable(
+      chip_start_ptr, m_type_->getArrayElementType(), chip_shape, this);
 
   this->m_children_.push_back(output);
 
@@ -103,9 +120,9 @@ Hobbit::Variable::GetChunk(llvm::IRBuilder<> &builder, const Hobbit::Range &k_ra
 }
 
 Hobbit::Variable *Hobbit::Variable::Flatten() {
-  Shape flat_shape (1, 1, m_shape_.GetSize());
+  Shape flat_shape(1, 1, m_shape_.GetSize());
 
-  Variable *output = new Variable (m_value_, m_type_, flat_shape, this);
+  Variable *output = new Variable(m_value_, m_type_, flat_shape, this);
 
   this->m_children_.push_back(output);
 
@@ -118,15 +135,14 @@ void Hobbit::Variable::ClearChild(Hobbit::Variable *child) {
   if (iter != m_children_.end()) {
     m_children_.erase(iter);
   }
-
 }
 
-void Hobbit::Variable::ClearChildren() {
-  this->m_children_.clear();
-}
+void Hobbit::Variable::ClearChildren() { this->m_children_.clear(); }
 
-llvm::ArrayRef<llvm::Value *> Hobbit::Variable::Pack(llvm::IRBuilder<> &builder, uint32_t &vector_size) {
-  llvm::Type *vector_type = llvm::VectorType::get(m_type_->getArrayElementType(), vector_size);
+llvm::ArrayRef<llvm::Value *> Hobbit::Variable::Pack(llvm::IRBuilder<> &builder,
+                                                     uint32_t &vector_size) {
+  llvm::Type *vector_type =
+      llvm::VectorType::get(m_type_->getArrayElementType(), vector_size);
   uint32_t leftovers = m_shape_.GetSize() % vector_size;
   uint32_t num_vectors = (m_shape_.GetSize() - leftovers) / vector_size;
 
@@ -134,7 +150,8 @@ llvm::ArrayRef<llvm::Value *> Hobbit::Variable::Pack(llvm::IRBuilder<> &builder,
   for (uint32_t i = 0; i < num_vectors; i++) {
     llvm::Value *tmp_vec = llvm::UndefValue::get(vector_type);
     for (uint32_t j = 0; j < vector_size; j++) {
-      llvm::Value *elt = builder.CreateExtractValue(m_value_, i * vector_size + j);
+      llvm::Value *elt =
+          builder.CreateExtractValue(m_value_, i * vector_size + j);
       builder.CreateInsertElement(tmp_vec, elt, j);
     }
     output.push_back(tmp_vec);
