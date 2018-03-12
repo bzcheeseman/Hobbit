20b7b2930762b7b1245fc6b814a3d1f2b6bfac5f
aman <aman@unify.id>
2018-03-12 09:17:13 -0700

    fix llvm hints for homebrew users

--- cmake/AddLLVM.cmake
+++ cmake/AddLLVM.cmake
@@ -1,4 +1,4 @@
-find_package(LLVM 6.0 REQUIRED CONFIG HINTS /usr/local/Cellar)
+find_package(LLVM 6.0 REQUIRED CONFIG HINTS /usr/local/Cellar/llvm/6.0.0)
 
 message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
 message(STATUS "Using LLVMConfig.cmake in: ${LLVM_DIR}")
